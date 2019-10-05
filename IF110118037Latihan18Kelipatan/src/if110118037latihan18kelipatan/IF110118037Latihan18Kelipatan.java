17212cf49c8d4544b400f9a375cfadd6c4f215e9
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-10-05 08:12:51 +0700

    Add files via upload

---
+++ IF110118037Latihan18Kelipatan/src/if110118037latihan18kelipatan/IF110118037Latihan18Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan18kelipatan;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program Kelipatan
+ */
+public class IF110118037Latihan18Kelipatan {
+
+    
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(i*3.5);
+        }
+        
+    }
+    
+}
